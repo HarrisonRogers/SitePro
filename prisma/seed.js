6a9e6132dd5bcca9c5a63cbb0c2b3b1ad3cde8f0
HarrisonRogers <harrison.rogers05@gmail.com>
2024-07-08 11:24:39 +1200

    seed more json data

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,8 +6,63 @@ async function main() {
   for (const site of data) {
     const { interiorProducts, exteriorProducts, ...siteData } = site
 
-    await prisma.site.create({
-      data: {
+    await prisma.site.upsert({
+      where: { id: site.id },
+      update: {
+        ...siteData,
+        createdAt: new Date(siteData.createdAt),
+        updatedAt: new Date(siteData.updatedAt),
+        buildStart: new Date(siteData.buildStart),
+        interiorProducts: {
+          deleteMany: {},
+          create: interiorProducts.map((product) => ({
+            name: product.name,
+            supplier: product.supplier,
+            maintenanceInstructions: {
+              create: product.maintenanceInstructions.map((instruction) => ({
+                actionRequired: instruction.actionRequired,
+                frequency: instruction.frequency,
+                dueOn: new Date(instruction.dueOn),
+              })),
+            },
+            installers: {
+              connectOrCreate: product.installers.map((installer) => ({
+                where: { id: installer.id },
+                create: {
+                  id: installer.id,
+                  name: installer.name,
+                  contact: installer.contact,
+                },
+              })),
+            },
+          })),
+        },
+        exteriorProducts: {
+          deleteMany: {},
+          create: exteriorProducts.map((product) => ({
+            name: product.name,
+            supplier: product.supplier,
+            maintenanceInstructions: {
+              create: product.maintenanceInstructions.map((instruction) => ({
+                actionRequired: instruction.actionRequired,
+                frequency: instruction.frequency,
+                dueOn: new Date(instruction.dueOn),
+              })),
+            },
+            installers: {
+              connectOrCreate: product.installers.map((installer) => ({
+                where: { id: installer.id },
+                create: {
+                  id: installer.id,
+                  name: installer.name,
+                  contact: installer.contact,
+                },
+              })),
+            },
+          })),
+        },
+      },
+      create: {
         ...siteData,
         createdAt: new Date(siteData.createdAt),
         updatedAt: new Date(siteData.updatedAt),
@@ -68,6 +123,7 @@ main()
     await prisma.$disconnect()
   })
   .catch(async (e) => {
+    console.error(e)
     await prisma.$disconnect()
     process.exit(1)
   })
