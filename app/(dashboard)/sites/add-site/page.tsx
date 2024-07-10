ed39b02c37d32ffa4148a5bb6a265abd617d0206
HarrisonRogers <harrison.rogers05@gmail.com>
2024-07-10 15:03:52 +1200

    start create form

--- app/(dashboard)/sites/add-site/page.tsx
+++ app/(dashboard)/sites/add-site/page.tsx
@@ -1,12 +1,20 @@
 import CreateSiteForm from '@/components/CreateSiteForm'
 import { Card } from '@/components/ui/card'
 import React from 'react'
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query'
 
 const addSite = () => {
+  const queryClient = new QueryClient()
   return (
-    <Card>
-      <CreateSiteForm />
-    </Card>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Card>
+        <CreateSiteForm />
+      </Card>
+    </HydrationBoundary>
   )
 }
 
--- components/CreateSiteForm.tsx
+++ components/CreateSiteForm.tsx
@@ -1,7 +1,79 @@
-import React from 'react'
+'use client'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import React, { useState } from 'react'
+import { useToast } from './ui/use-toast'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { CreateSite } from '@/utils/types'
+import { createSiteAction } from '@/utils/actions'
+import { Form } from './ui/form'
+import { CustomFormField, CustomFormSelect } from './FormComponents'
+import { Checkbox } from './ui/checkbox'
 
 const CreateSiteForm = () => {
-  return <div>CreateSiteForm</div>
+  const queryClient = useQueryClient()
+  // const [formValues, setFormValues] = useState()
+  const { toast } = useToast()
+  const router = useRouter()
+  const form = useForm<CreateSite>({
+    defaultValues: {
+      jobReference: '',
+      owners: '',
+      siteAddress: '',
+      buildComplete: false,
+      buildStart: '',
+    },
+  })
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (values: CreateSite) => createSiteAction(values),
+    onSuccess: (data) => {
+      if (!data) {
+        toast({ description: 'there was an error' })
+        return
+      }
+      toast({ description: 'job created' })
+      queryClient.invalidateQueries({ queryKey: ['sites'] })
+
+      router.push('/sites')
+    },
+  })
+
+  const onSubmit = (values: CreateSite) => {
+    mutate(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="bg-primary p-8 rounded text-white"
+      >
+        <h2 className="capitalize font-semibold text-4xl mb-6">Add Site</h2>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 items-start">
+          {/* Refernce */}
+          <CustomFormField name="Job reference" control={form.control} />
+          {/* owners */}
+          <CustomFormField name="Owners" control={form.control} />
+          {/* Site address */}
+          <CustomFormField name="Site Address" control={form.control} />
+          {/* Build start */}
+          {/* Needs complete */}
+
+          {/* build complete */}
+          <div className="flex items-center space-x-2">
+            <Checkbox id="complete" className="border-white" />
+            <label
+              htmlFor="complete"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Is complete
+            </label>
+          </div>
+        </div>
+      </form>
+    </Form>
+  )
 }
 
 export default CreateSiteForm
